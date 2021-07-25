cd5b28afa8ac5f90fb539bf309dc556359e9886a
Mike543 <44565608+Mike543@users.noreply.github.com>
2021-07-25 10:54:45 +0300

    Commit_3
    JS mod

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ let appData = {
     timeData: time,
     savings: false
 };
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert("Важ среднеарифметический бюджет на день =" + appData.budget / 30);
